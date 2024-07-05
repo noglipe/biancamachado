0578d758e1e3993f1c2d49065f02990771349bfc
Filipe Nogueira <nog.lipe@gmail.com>
2024-07-05 14:37:40 -0300

    dev pagina Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import { PainelFotos } from "@/components/painelFotos";
+
+import fotoPerfil from '@/assets/perfil.jpg'
+
+import { MdOutlinePhotoCamera } from "react-icons/md";
+import { BsInstagram } from "react-icons/bs";
+import { SiWhatsapp } from "react-icons/si";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <div>
+
+      <div className={styles.container} >
+        <div className={styles.apresentacao} >
+          <div >
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src={fotoPerfil}
+              alt={"Foto perfil Bianca "}
+              layout="responsive"
+              style={{ minWidth: 80, minHeight: 80, maxWidth: 200, maxHeight: 200, borderRadius: 100 }}
             />
-          </a>
+          </div>
+          <div className={styles.info} >
+            <h1 className={styles.titulo}>
+              BIANCA MACHADO
+            </h1>
+            <h3 className={styles.subtitulo}>
+              <Link href="https://wa.me/5527981655844" target='_blank'><SiWhatsapp size={30} color="green" /></Link >
+              <Link href="https://www.instagram.com/biancamachadophoto/" target='_blank'>
+                <BsInstagram size={30} />
+              </Link >
+            </h3 >
+          </div>
         </div>
+
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+      <PainelFotos />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
---
+++ src/components/painelFotos/index.tsx
@@ -0,0 +1,145 @@
+import styles from "./index.module.css";
+import Image from 'next/image';
+
+import foto1 from '@/assets/1.jpg';
+import foto2 from '@/assets/2.jpg';
+import foto3 from '@/assets/3.jpg';
+import foto4 from '@/assets/4.jpg';
+import foto5 from '@/assets/5.jpg';
+import foto6 from '@/assets/6.jpg';
+import foto7 from '@/assets/7.jpg';
+import foto8 from '@/assets/8.jpg';
+import foto9 from '@/assets/9.jpg';
+import foto10 from '@/assets/10.jpg';
+import foto11 from '@/assets/11.jpg';
+import foto12 from '@/assets/12.jpg';
+
+export function PainelFotos() {
+    return (
+        <div className={styles.painel}>
+
+            {/* Painel 1 */}
+            <div className={styles.coluna}>
+                <div className={styles.foto1x1}>
+                    <Image
+                        src={foto1}
+                        alt="Foto do Painel de Fotos"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+                <div className={styles.foto1x1}>
+                    <Image
+                        src={foto2}
+                        alt="Foto do Painel de Fotos"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+
+            </div>
+            <div className={styles.coluna}>
+                <div className={styles.foto2x2}>
+                    <Image
+                        src={foto3}
+                        alt="Foto do Painel de Fotos"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+            </div>
+
+            {/* Painel interemediario */}
+            <div className={styles.box1}>
+                <Image
+                    src={foto4}
+                    alt="Foto do Painel de Fotos"
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </div>
+
+            {/* Painel 2 */}
+            <div className={styles.coluna}>
+                <div className={styles.foto1x1}>
+                    <Image
+                        src={foto5}
+                        alt="Foto do Painel de Fotos"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+                <div className={styles.foto1x1}>
+                    <Image
+                        src={foto6}
+                        alt="Foto do Painel de Fotos"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+
+            </div>
+            <div className={styles.coluna}>
+                <div className={styles.foto1x1}>
+                    <Image
+                        src={foto7}
+                        alt="Foto do Painel de Fotos"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+                <div className={styles.foto1x1}>
+                    <Image
+                        src={foto8}
+                        alt="Foto do Painel de Fotos"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+            </div>
+
+
+            {/* Painel 3 */}
+            <div className={styles.coluna}>
+                <div className={styles.foto2x2}>
+                    <Image
+                        src={foto9}
+                        alt="Foto do Painel de Fotos"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+            </div>
+            <div className={styles.coluna}>
+                <div className={styles.foto1x1}>
+                    <Image
+                        src={foto10}
+                        alt="Foto do Painel de Fotos"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+                <div className={styles.foto1x1}>
+                    <Image
+                        src={foto11}
+                        alt="Foto do Painel de Fotos"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+
+            </div>
+
+            {/* Painel interemediario */}
+            <div className={styles.box1}>
+                <Image
+                    src={foto12}
+                    alt="Foto do Painel de Fotos"
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </div>
+
+        </div>
+    );
+}
